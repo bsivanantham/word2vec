83025c95c4ceaa54244afeb6c61c3be3ddaa01cc
bsivanantham <balavivek107@gmail.com>
2018-10-28 20:41:06 +0100

    simple gui merged and trained w2v

--- sliders.py
+++ sliders.py
@@ -1,60 +1,18 @@
-import numpy as np
-
-from bokeh.io import curdoc
-from bokeh.layouts import row, widgetbox
-from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Slider, TextInput
-from bokeh.plotting import figure
-
-# Set up data
-N = 200
-x = np.linspace(0, 4*np.pi, N)
-y = np.sin(x)
-source = ColumnDataSource(data=dict(x=x, y=y))
+from guizero import App, Slider, TextBox
 
 
-# Set up plot
-plot = figure(plot_height=400, plot_width=400, title="my sine wave",
-              tools="crosshair,pan,reset,save,wheel_zoom",
-              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
+def slider_changed1(slider_value):
+    textbox1.value = slider_value
 
-plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-
-
-# Set up widgets
-text = TextInput(title="title", value='my sine wave')
-offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
-amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
-phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
-freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
+def slider_changed2(slider_value):
+    textbox2.value = slider_value
 
 
-# Set up callbacks
-def update_title(attrname, old, new):
-    plot.title.text = text.value
+app = App(title="Reinforcement Learning")
+slider1 = Slider(app, command=slider_changed1)
+textbox1 = TextBox(app)
 
-text.on_change('value', update_title)
+slider2 = Slider(app, command=slider_changed2)
+textbox2 = TextBox(app)
 
-def update_data(attrname, old, new):
-
-    # Get the current slider values
-    a = amplitude.value
-    b = offset.value
-    w = phase.value
-    k = freq.value
-
-    # Generate the new curve
-    x = np.linspace(0, 4*np.pi, N)
-    y = a*np.sin(k*x + w) + b
-
-    source.data = dict(x=x, y=y)
-
-for w in [offset, amplitude, phase, freq]:
-    w.on_change('value', update_data)
-
-
-# Set up layouts and add to document
-inputs = widgetbox(text, offset, amplitude, phase, freq)
-
-curdoc().add_root(row(inputs, plot, width=800))
-curdoc().title = "Sliders"
+app.display()
--- word2vec.py
+++ word2vec.py
@@ -4,17 +4,20 @@ from collections import defaultdict
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import pandas as pd
+from guizero import App, Slider, TextBox
+import dill
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 class word2vec():
-    def __init__ (self):
+    def __init__(self):
         self.n = settings['n']
         self.eta = settings['learning_rate']
         self.epochs = settings['epochs']
         self.window = settings['window_size']
         pass
-    
-    
+
     # GENERATE TRAINING DATA
     def generate_training_data(self, settings, corpus):
 
@@ -27,7 +30,7 @@ class word2vec():
         self.v_count = len(word_counts.keys())
 
         # GENERATE LOOKUP DICTIONARIES
-        self.words_list = sorted(list(word_counts.keys()),reverse=False)
+        self.words_list = sorted(list(word_counts.keys()), reverse=False)
         self.word_index = dict((word, i) for i, word in enumerate(self.words_list))
         self.index_word = dict((i, word) for i, word in enumerate(self.words_list))
 
@@ -38,24 +41,22 @@ class word2vec():
 
             # CYCLE THROUGH EACH WORD IN SENTENCE
             for i, word in enumerate(sentence):
-                
-                #w_target  = sentence[i]
+
+                # w_target  = sentence[i]
                 w_target = self.word2onehot(sentence[i])
 
                 # CYCLE THROUGH CONTEXT WINDOW
                 w_context = []
-                for j in range(i-self.window, i+self.window+1):
-                    if j!=i and j<=sent_len-1 and j>=0:
+                for j in range(i - self.window, i + self.window + 1):
+                    if j != i and j <= sent_len - 1 and j >= 0:
                         w_context.append(self.word2onehot(sentence[j]))
                 training_data.append([w_target, w_context])
         return np.array(training_data)
-
 
     # SOFTMAX ACTIVATION FUNCTION
     def softmax(self, x):
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum(axis=0)
-
 
     # CONVERT WORD TO ONE HOT ENCODING
     def word2onehot(self, word):
@@ -64,18 +65,16 @@ class word2vec():
         word_vec[word_index] = 1
         return word_vec
 
-
     # FORWARD PASS
     def forward_pass(self, x):
         h = np.dot(self.w1.T, x)
         u = np.dot(self.w2.T, h)
         y_c = self.softmax(u)
         return y_c, h, u
-                
 
     # BACKPROPAGATION
     def backprop(self, e, h, x):
-        dl_dw2 = np.outer(h, e)  
+        dl_dw2 = np.outer(h, e)
         dl_dw1 = np.outer(x, np.dot(self.w2, e.T))
 
         # UPDATE WEIGHTS
@@ -83,13 +82,12 @@ class word2vec():
         self.w2 = self.w2 - (self.eta * dl_dw2)
         pass
 
-
     # TRAIN W2V model
     def train(self, training_data):
         # INITIALIZE WEIGHT MATRICES
-        self.w1 = np.random.uniform(-0.8, 0.8, (self.v_count, self.n))     # context matrix
-        self.w2 = np.random.uniform(-0.8, 0.8, (self.n, self.v_count))     # embedding matrix
-        
+        self.w1 = np.random.uniform(-0.8, 0.8, (self.v_count, self.n))  # context matrix
+        self.w2 = np.random.uniform(-0.8, 0.8, (self.n, self.v_count))  # embedding matrix
+
         # CYCLE THROUGH EACH EPOCH
         for i in range(0, self.epochs):
 
@@ -97,10 +95,9 @@ class word2vec():
 
             # CYCLE THROUGH EACH TRAINING SAMPLE
             for w_t, w_c in training_data:
-
                 # FORWARD PASS
                 y_pred, h, u = self.forward_pass(w_t)
-                
+
                 # CALCULATE ERROR
                 EI = np.sum([np.subtract(y_pred, word) for word in w_c], axis=0)
 
@@ -109,18 +106,17 @@ class word2vec():
 
                 # CALCULATE LOSS
                 self.loss += -np.sum([u[word.index(1)] for word in w_c]) + len(w_c) * np.log(np.sum(np.exp(u)))
-                self.loss += -2*np.log(len(w_c)) -np.sum([u[word.index(1)] for word in w_c]) + (len(w_c) * np.log(np.sum(np.exp(u))))
-                
-            print('EPOCH:',i, 'LOSS:', self.loss)
-        pass
+                self.loss += -2 * np.log(len(w_c)) - np.sum([u[word.index(1)] for word in w_c]) + (
+                            len(w_c) * np.log(np.sum(np.exp(u))))
 
+            print('EPOCH:', i, 'LOSS:', self.loss)
+        pass
 
     # input a word, returns a vector (if available)
     def word_vec(self, word):
         w_index = self.word_index[word]
         v_w = self.w1[w_index]
         return v_w
-
 
     # input a vector, returns nearest word(s)
     def vec_sim(self, vec, top_n):
@@ -136,15 +132,15 @@ class word2vec():
             word = self.index_word[i]
             word_sim[word] = theta
 
-        words_sorted = sorted(word_sim.items(), key=lambda word, sim:sim, reverse=True)
+        words_sorted = sorted(word_sim.items(), key=lambda word, sim: sim, reverse=True)
         for word, sim in words_sorted[:top_n]:
-            print('vec_sim',word, sim)
-            
+            print('vec_sim', word, sim)
+
         pass
 
     # input word, returns top [n] most similar words
     def word_sim(self, word, top_n):
-        
+
         w1_index = self.word_index[word]
         v_w1 = self.w1[w1_index]
 
@@ -159,29 +155,22 @@ class word2vec():
             word = self.index_word[i]
             word_sim[word] = theta
 
-        words_sorted = sorted(word_sim.items(), key=lambda word, sim:sim, reverse=True)
+        words_sorted = sorted(word_sim.items(), key=lambda word, sim: sim, reverse=True)
 
         for word, sim in words_sorted[:top_n]:
-            print('word_sim',word, sim)
-            
+            print('word_sim', word, sim)
+
         pass
-
-#--- EXAMPLE RUN --------------------------------------------------------------+
-
-import dill
-import numpy as np
-from sklearn.model_selection import train_test_split
 
 
 with open('motion_capture_20181011-1931.dill', 'rb') as f:
     x = dill.load(f)
 vec = [l[4] for l in x]
-#print(len(vec))
+# print(len(vec))
 
 x = map(str, vec)
 x = list(x)
-print(type(x))
-print(len(x))
+
 
 def remove_stop_words(corpus):
     stop_words = [', ']
@@ -195,17 +184,18 @@ def remove_stop_words(corpus):
     print(results)
     return results
 
-x = remove_stop_words(x)
-X_train , X_test = train_test_split(x,test_size = 0.33 , shuffle =False)
+
+#x = remove_stop_words(x)
+X_train, X_test = train_test_split(x, test_size=0.33, shuffle=False)
 
 settings = {}
-settings['n'] = 5                   # dimension of word embeddings
-settings['window_size'] = 2         # context window +/- center word
-settings['min_count'] = 0           # minimum word count
-settings['epochs'] = 1000           # number of training epochs
-settings['neg_samp'] = 10           # number of negative words to use during training
-settings['learning_rate'] = 0.01    # learning rate
-np.random.seed(0)                   # set the seed for reproducibility
+settings['n'] = 2  # dimension of word embeddings
+settings['window_size'] = 2  # context window +/- center word
+settings['min_count'] = 0  # minimum word count
+settings['epochs'] = 10  # number of training epochs
+settings['neg_samp'] = 10  # number of negative words to use during training
+settings['learning_rate'] = 0.01  # learning rate
+np.random.seed(0)  # set the seed for reproducibility
 
 corpus = [X_train]
 
@@ -218,10 +208,9 @@ training_data = w2v.generate_training_data(settings, corpus)
 # train word2vec model
 w2v.train(training_data)
 
-print ('Training data length',len(training_data))
-print ('Training data 0',training_data[0])
+print('Training data length', len(training_data))
+print('Training data 0', training_data[0])
 
+print ('Vector of the word',w2v.word_vec(X_test[0]))
 
-#print ('Vector of the word',w2v.word_vec('the'))
-
-#--- END ----------------------------------------------------------------------+
+# --- END ----------------------------------------------------------------------+
