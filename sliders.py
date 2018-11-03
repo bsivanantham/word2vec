557541e25ff95a4504050e5b7ac6bd7f79571acb
bsivanantham <balavivek107@gmail.com>
2018-11-03 03:19:20 +0100

    GUI with TK and fixed decimal point issue in slider

--- sliders.py
+++ sliders.py
@@ -1,19 +1,61 @@
-from guizero import App, Slider, TextBox
+from modelrestore import v_
 
-slider1value = 0
-slider2value = 0
-def slider_changed1(slider_value):
-    textbox1.value = slider_value
+#print(v_)
+from tkinter import *
+import tkinter as tk
+import locale
 
-def slider_changed2(slider_value):
-    textbox2.value = slider_value
+class NewScale(tk.Frame):
+    def __init__(self, master=None, **options):
+        tk.Frame.__init__(self, master)
+
+        # Disable normal value display...
+        options['showvalue'] = False
+        # ... and use custom display instead
+        options['command'] = self._on_scale
+
+        # Set resolution to 1 and adjust to & from value
+        self.res = options.get('resolution', 1)
+        from_ = int(options.get('from_', 0) / self.res)
+        to = int(options.get('to', 100) / self.res)
+        options.update({'resolution': 1, 'to': to, 'from_': from_})
+
+        # This could be improved...
+        if 'digits' in options:
+            self.digits = ['digits']
+            del options['digits']
+        else:
+            self.digits = 2
+
+        self.scale = tk.Scale(self, **options)
+        self.scale_label = tk.Label(self)
+        orient = options.get('orient', tk.VERTICAL)
+        if orient == tk.VERTICAL:
+            side, fill = 'right', 'y'
+        else:
+            side, fill = 'top', 'x'
+        self.scale.pack(side=side, fill=fill)
+        self.scale_label.pack(side=side)
+
+    def _on_scale(self, value):
+        value = locale.atof(value) * self.res
+        value = locale.format_string('%.*f', (self.digits, value))
+        self.scale_label.configure(text=value)
+
+if __name__ == '__main__':
+    master = tk.Tk()
+    master.geometry("500x500")
+    display_text = tk.StringVar()
+    display = tk.Label(master, textvariable=display_text)
 
 
-app = App(title="Reinforcement Learning")
-slider1 = Slider(app, command=slider_changed1)
-textbox1 = TextBox(app)
+    w1 = NewScale(master, from_=-2, to=1, resolution=0.000001)
+    w2 = NewScale(master, from_=-2, to=1, resolution=0.000001)
+    T = Text(master, height=2, width=30)
+    w1.pack(side=LEFT, expand=True)
+    w2.pack(side=LEFT, expand=True)
+    T.pack(side=LEFT,padx=10)
+    display.pack()
 
-slider2 = Slider(app, command=slider_changed2)
-textbox2 = TextBox(app)
-
-app.display()
+    display_text.set("Output vector")
+    master.mainloop()
