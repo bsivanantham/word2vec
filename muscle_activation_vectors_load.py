d2283d3616633c503e17fa5cf7e1207a0e5369a7
bsivanantham <balavivek107@gmail.com>
2018-11-24 13:01:50 +0100

    trained for 30000 epochs

--- muscle_activation_vectors_load.py
+++ muscle_activation_vectors_load.py
@@ -6,16 +6,52 @@ with open('motion_capture_20181011-1931.dill', 'rb') as f:
     x = dill.load(f)
 vec = [l[4] for l in x]
 
-print(len(vec))
 
+arra =[]
+x =[]
+z =[]
+flag = False
 # vec = vec.tolist()
-vec = [x.tolist() for x in vec]
-vec = [(list(y)) for y in set(tuple(x) for x in vec)]
+#vec = [[1,1,1,2],[1,1,1,2],[2,2,4,4,3],[2,2,4,4,3],[2,2,4,4,3],[1,1,1,2],[1,1,1,2],[1,1,1,2]]
+# for i in range(len(vec)):
+#     for j in range(len(vec)-1):
+#         if((vec[i] != vec[j]).all()):
+#             flag = True
+#             #arra.append(vec[i])
+#         else:
+#             flag = False
+#             #x.append(vec[i])
+#     if(flag):
+#         arra.append(vec[i-1])
+#         arra.append(vec[i])
+#     else:
+#         x.append(vec[i])
+d = []
+count =0
+# for i in range (len(vec)):
+#     for j in range(len(vec)-1):
+#         arra = vec[i]
+#         if((arra == vec[j]).all()):
+#             count = count+1
+#         else:
+#             arra = vec[j]
+#     if (count> 2):
+#         z.append(vec[i])
+#     else:
+#         x.append(vec[i])
+arr = vec[0]
+z.append(vec[0])
+for i in range (len(vec)):
+    if((arr != vec[i]).any()):
+        z.append(vec[i])
+        arr = vec[i]
 
-vec = [list(x) for x in vec]
-vec = np.array(vec)
-vec = np.round(vec)
+# vec = [(list(y)) for y in set(tuple(x) for x in vec)]
+#
+# vec = [list(x) for x in vec]
+z = np.array(z)
+z = np.round(z)
 print(len(vec))
-
+print(z)
 with open("resultround.dill", "wb") as dill_file:
-    dill.dump(vec, dill_file)
+    dill.dump(z, dill_file)
